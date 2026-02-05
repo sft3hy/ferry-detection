186db8f048a1b2016004768e7fe4425f3e3f35a5
Sam Townsend <sft3hy@virginia.edu>
2026-02-05 10:22:22 -0800

    do not save images

--- config/settings.py
+++ config/settings.py
@@ -55,9 +55,9 @@ LOG_DETECTIONS = False
 LOG_FILE = "ferry_detections.log"
 
 # Image saving (optional)
-SAVE_IMAGES = True
+SAVE_IMAGES = False
 SAVE_DIR = "detected_images"
-SAVE_ALL_CAPTURES = True  # Debug: save all downloaded images regardless of detection
+SAVE_ALL_CAPTURES = False  # Debug: save all downloaded images regardless of detection
 
 
 CS_HOST = "chatsurfer.nro.mil"
